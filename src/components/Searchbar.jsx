c9df740ba9cc512b62963a93199b41d629b6f294
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2023-03-01 13:51:44 +0100

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { FiSearch } from 'react-icons/fi';
@@ -14,15 +13,24 @@ const Searchbar = () => {
   };
 
   return (
-       <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
       <label htmlFor="search-field" className="sr-only">
-        Search all songs
+        Search all files
       </label>
       <div className="flex flex-row justify-start items-center">
-        <FiSearch className="w-5 h-5 ml-4" />
-        <input type="search" name="search-field" autoComplete="off" id="search-field" placeholder="Search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4" />
+        <FiSearch aria-hidden="true" className="w-5 h-5 ml-4" />
+        <input
+          name="search-field"
+          autoComplete="off"
+          id="search-field"
+          className="flex-1 bg-transparent border-none placeholder-gray-500 outline-none text-base text-white p-4"
+          placeholder="Search"
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </div>
-    </form> 
+    </form>
   );
 };
 
