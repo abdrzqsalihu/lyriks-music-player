a49a39761ce7d45995c8f4405a3d8fa814310fbb
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2023-03-01 12:55:51 +0100

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -14,15 +14,17 @@ const Searchbar = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+   <div className="mt-[200px]">
+       <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
       <label htmlFor="search-field" className="sr-only">
         Search all songs
       </label>
       <div className="flex flex-row justify-start items-center">
         <FiSearch className="w-5 h-5 ml-4" />
-        <input type="search" name="search-field" autoComplete="off" id="search-field" placeholder="Search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="flex-1  bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4" />
+        <input type="search" name="search-field" autoComplete="off" id="search-field" placeholder="Search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4" />
       </div>
-    </form>
+    </form> 
+   </div>
   );
 };
 
