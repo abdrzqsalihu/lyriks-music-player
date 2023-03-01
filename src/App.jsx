14084b58d3fc10eeed544ce85146169d47f8cf05
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2023-03-01 14:06:05 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ const App = () => {
 
   return (
     <div className="relative flex">
-      <Sidebar />
+        <Sidebar className="z-20" />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-        <Searchbar />
+        <Searchbar className="z-20" />
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
